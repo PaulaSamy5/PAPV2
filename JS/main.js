a8617bcef4ea94418aeae1b25c0a07382a73efda
PaulaSamy5 <paulasamy52@gmial.com>
2024-09-10 14:50:12 +0300

    first commit

--- JS/main.js
+++ JS/main.js
@@ -2,31 +2,39 @@ function myfunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-// Check if dark mode preference is stored in local storage
-const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
 
-// Apply dark mode on page load
-if (isDarkMode) {
-    enableDarkMode();
-}
 
-// Function to toggle dark mode
-function toggleDarkMode() {
-    if (isDarkMode) {
-        disableDarkMode();
+    // Check if dark mode is enabled in localStorage when the page loads
+    window.onload = function() {
+        if (localStorage.getItem('darkMode') === 'enabled') {
+            document.body.classList.add('dark-mode');
+        }
+    };
+
+    function myfunction() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+
+        // Save the current mode to localStorage
+        if (element.classList.contains("dark-mode")) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    }
+    let scrolll = document.getElementById('scroll')
+window.onscroll = function () {
+    if (scrollY >= 400) {
+        scrolll.style.display = "block";
     } else {
-        enableDarkMode();
+        scrolll.style.display = "none";
     }
 }
 
-// Function to enable dark mode
-function enableDarkMode() {
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('darkMode', 'enabled');
-}
-
-// Function to disable dark mode
-function disableDarkMode() {
-    document.body.classList.remove('dark-mode');
-    localStorage.setItem('darkMode', null);
+function upScroll() {
+    scroll({
+        left: 0,
+        top: 0,
+        behavior: "smooth",
+    })
 }
